4dc06e4814718f4612ac5856c93a71d592813553
Sakthi <82046832+0xsakthi@users.noreply.github.com>
2021-08-17 20:58:28 +0530

    Add files via upload

---
+++ looping/sum-of-digits.py
@@ -0,0 +1,14 @@
+#Write a program to display sum of digits of a number
+#!/usr/bin/python3
+
+
+def sumofdigit():
+	ans = 0
+	for i in num:
+		inte = int(i)
+		ans += inte 
+	print(ans)
+
+num  = '1234'
+
+sumofdigit(num)
