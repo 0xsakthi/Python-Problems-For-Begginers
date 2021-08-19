578015db4542ba0c4e63d809e4b39d84d780f730
Sakthi <82046832+0xsakthi@users.noreply.github.com>
2021-08-19 18:42:29 +0530

    Add files via upload

---
+++ Basic-Programming/circulate-the-values-of-n-var.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+input1 = int(input("Enter the number: "))
+list1 = []
+
+for i in range(0,input1):
+    input2 = int(input("Enter the adding number: "))
+    list1.append(input2)
+
+print(list1)
+
+for j in range(0,input1-1):
+    print(list1)
---
+++ Basic-Programming/swap-two-number.py
@@ -0,0 +1,9 @@
+#Swap two numbers?
+#!/usr/bin/python3
+
+A = input("A: ")
+B = input("B: ")
+print(A,B)
+print('After Swap')
+A,B = B,A
+print("A ={0} ,B = {1}".format(A,B))
